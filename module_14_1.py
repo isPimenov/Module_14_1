1806e7baaec8d0837da6f419b23927c65c2e6de4
isPimenov <ipimenow@gmail.com>
2024-12-06 22:21:45 +0300

    Update

--- module_14_1.py
+++ module_14_1.py
@@ -15,14 +15,13 @@ for i in range(1, 11):
     cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?,?)',
                    (f'User{i}', f'example{i}@gmail.com', i * 10, 1000))
 
+cursor.execute('UPDATE Users SET balance = 500 WHERE id % 2 != 0')
 
-for i in range(1, 11):
-    if i % 2 != 0:
-        cursor.execute('UPDATE Users SET balance = 500 WHERE username = ?', (f'User{i}',))
+
 
 i = 1
 while i < 11:
-    cursor.execute('DELETE FROM Users WHERE username = ?', (f'User{i}',))
+    cursor.execute(f'DELETE FROM Users WHERE id = {i}')
     i += 3
 
 cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
